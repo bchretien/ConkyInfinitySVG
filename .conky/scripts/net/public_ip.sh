68b86c77fa0eef0ea032600252a52542363361af
Benjamin Chrétien <chretien@lirmm.fr>
2013-12-29 17:12:42 +0100

    Remove scripts that were moved to conky-themes.

--- .conky/scripts/net/public_ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PUBLIC_IP=`wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>'`
-if [ x${PUBLIC_IP} = x ]; then
-    echo "No Address"
-else
-    echo $PUBLIC_IP
-fi
